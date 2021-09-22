52a0777059eab16a845e9e41e34b831336cab89d
priithika <73788250+priithika@users.noreply.github.com>
2021-09-22 15:35:18 +0530

    Add files via upload

---
+++ two.m
@@ -0,0 +1,10 @@
+clc
+close all
+g=alexnet;
+layers=g.Layers;
+layers(23)=fullyConnectedLayer(2);
+layers(25)=classificationLayer;
+allImages=imageDatastore('database','IncludeSubfolders',true, 'LabelSource','foldernames');
+opts=trainingOptions('sgdm','InitialLearnRate',0.001,'MaxEpochs',20,'MiniBatchSize',64);
+myNet=trainNetwork(allImages,layers,opts);
+save myNet;
